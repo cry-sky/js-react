c6b3664186a3292a5e290a59ad94ec1cce822406
crysky <9deletedata@gmail.com>
2020-04-08 20:18:51 +0300

    Updates

---
+++ precache-manifest.4bd46b7e879f9a83fd71f9e1ed3fb93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8861435745c7c11f6de424a429ebd0",
+    "url": "/cry-sky/js-react/tree/movie-app/index.html"
+  },
+  {
+    "revision": "cc48e8cc28b5b519d86e",
+    "url": "/cry-sky/js-react/tree/movie-app/static/css/main.f2a94455.chunk.css"
+  },
+  {
+    "revision": "40c7c8d5cea4507b73c6",
+    "url": "/cry-sky/js-react/tree/movie-app/static/js/2.19ff9cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cry-sky/js-react/tree/movie-app/static/js/2.19ff9cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc48e8cc28b5b519d86e",
+    "url": "/cry-sky/js-react/tree/movie-app/static/js/main.2c761b8c.chunk.js"
+  },
+  {
+    "revision": "8285f3e5facf714caf02",
+    "url": "/cry-sky/js-react/tree/movie-app/static/js/runtime-main.c61027e7.js"
+  }
+]);
